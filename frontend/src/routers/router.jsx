4031b3e57641cf1a77595a5c505d014e9aa66de4
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2024-11-13 02:43:59 +0600

    Category Based Product Showcasing

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,16 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
+import CategoryPage from "../pages/category/CategoryPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
     children: [
-        {
-            path: "/",
-            element: <Home></Home>,
-        }
+      { path: "/", element: <Home></Home> },
+      {
+        path: "/categories/:categoryName",
+        element: <CategoryPage></CategoryPage>
+      },
     ],
   },
 ]);
