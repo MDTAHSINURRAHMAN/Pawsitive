01f5ad94884e4c1aafa2571d74557db4b58387fb
MDTAHSINURRAHMAN <mdtahsinurrahman1210@gmail.com>
2024-11-13 13:35:10 +0600

    Redux Add korlam but kaj hoyna

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const products = useSelector((state) => state.cart.products);
+  console.log(products)
   return (
     <div>
       <header className="fixed-nav-bar w-nav">
